1e1849b01ecc10564cff67ab8437dcfb0fb4cc2a
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2016-01-08 21:26:28 +0200

    cosmetics

--- private/edtaa3func.nim
+++ private/edtaa3func.nim
@@ -8,11 +8,11 @@ import sequtils, math
 # The gradient is computed only at edge pixels. At other places in the
 # image, it is never used, and it's mostly zero anyway.
 proc computegradient(img: openarray[cdouble], w, h: cint, gx, gy: var openarray[cdouble]) =
-    const SQRT2 = cdouble(1.4142136)
+    const SQRT2 = 1.4142136
     for i in 1 ..< h - 1: # Avoid edges where the kernels would spill over
         for j in 1 ..< w - 1:
             let k = i * w + j
-            if (img[k] > 0) and (img[k] < 1): # Compute gradient for edge pixels only
+            if img[k] > 0 and img[k] < 1: # Compute gradient for edge pixels only
                 gx[k] = -img[k-w-1] - SQRT2*img[k-1] - img[k+w-1] + img[k-w+1] + SQRT2*img[k+1] + img[k+w+1]
                 gy[k] = -img[k-w-1] - SQRT2*img[k-w] - img[k+w-1] + img[k-w+1] + SQRT2*img[k+w] + img[k+w+1]
                 var glength = gx[k]*gx[k] + gy[k]*gy[k]
@@ -32,7 +32,7 @@ proc computegradient(img: openarray[cdouble], w, h: cint, gx, gy: var openarray[
 # accuracy at and near edges, and reduces the error even at distant pixels
 # provided that the gradient direction is accurately estimated.
 proc edgedf(gx, gy, a: cdouble): cdouble =
-    if (gx == 0) or (gy == 0): # Either A) gx or gv are zero, or B) both
+    if gx == 0 or gy == 0: # Either A) gx or gv are zero, or B) both
         result = 0.5 - a  # Linear approximation is A) correct or B) a fair guess
     else:
         let glength = sqrt(gx*gx + gy*gy)
@@ -47,8 +47,7 @@ proc edgedf(gx, gy, a: cdouble): cdouble =
 
         ggx = abs(ggx)
         ggy = abs(ggy)
-        if ggx < ggy:
-            swap(ggx, ggy)
+        if ggx < ggy: swap(ggx, ggy)
         let a1 = 0.5 * ggy / ggx
         if a < a1: # 0 <= a < a1
             result = 0.5 * (ggx + ggy) - sqrt(2.0 * ggx * ggy * a)
@@ -63,14 +62,14 @@ proc distaa3(img, gximg, gyimg: openarray[cdouble], w, c, xc, yc, xi, yi: cint):
     let gx = gximg[closest] # X gradient component at the edge pixel
     let gy = gyimg[closest] # Y gradient component at the edge pixel
 
-    if(a > 1.0): a = 1.0
-    if(a < 0.0): a = 0.0 # Clip grayscale values outside the range [0,1]
-    if(a == 0.0): return 1000000.0 # Not an object pixel, return "very far" ("don't know yet")
+    if a > 1.0: a = 1.0
+    if a < 0.0: a = 0.0 # Clip grayscale values outside the range [0,1]
+    if a == 0.0: return 1000000.0 # Not an object pixel, return "very far" ("don't know yet")
 
     let dx = xi.cdouble
     let dy = yi.cdouble
     let di = sqrt(dx*dx + dy*dy) # Length of integer vector, like a traditional EDT
-    if(di==0): # Use local gradient only at edges
+    if di == 0: # Use local gradient only at edges
         # Estimate based on local gradient only
         result = edgedf(gx, gy, a)
     else:
@@ -101,8 +100,7 @@ proc edtaa3(img, gx, gy: openarray[cdouble], w, h: cint, distx, disty: var opena
 
     # Initialize the distance images
     let sz = w * h
-    var i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         distx[i] = 0 # At first, all pixels point to
         disty[i] = 0 # themselves as the closest known.
         if img[i] <= 0:
@@ -111,7 +109,6 @@ proc edtaa3(img, gx, gy: openarray[cdouble], w, h: cint, distx, disty: var opena
             dist[i] = edgedf(gx[i], gy[i], img[i]) # Gradient-assisted estimate
         else:
             dist[i]= 0 # Inside the object
-        inc i
 
     var changed = true
 
@@ -464,73 +461,56 @@ proc make_distance_map*(data: var openarray[cdouble], width, height : cuint) =
     var img_max = -255.cdouble
 
     # Convert img into double (data)
-    var i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         let v = data[i]
         if v > img_max: img_max = v
         if v < img_min: img_min = v
-        inc i
 
     # Rescale image levels between 0 and 1
-    i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         data[i] = (data[i] - img_min) / img_max
-        inc i
 
     # Compute outside = edtaa3(bitmap); % Transform background (0's)
     computegradient(data, width.cint, height.cint, gx, gy)
     edtaa3(data, gx, gy, width.cint, height.cint, xdist, ydist, outside)
 
-    i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         if outside[i] < 0:
             outside[i] = 0
-        inc i
 
     # Compute inside = edtaa3(1-bitmap); % Transform foreground (1's)
     gx.applyIt(0)
     gy.applyIt(0)
 
-    i = 0
-    while i < sz:
-        data[i] = 1 - data[i]
-        inc i
+    for i in 0 ..< sz: data[i] = 1 - data[i]
 
     computegradient(data, width.cint, height.cint, gx, gy)
     edtaa3(data, gx, gy, width.cint, height.cint, xdist, ydist, inside)
 
-    i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         if inside[i] < 0:
             inside[i] = 0
-        inc i
 
     # distmap = outside - inside; % Bipolar distance field
-    i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         outside[i] -= inside[i]
         outside[i] = 128+outside[i]*16
         if outside[i] < 0: outside[i] = 0
         if outside[i] > 255: outside[i] = 255
         data[i] = outside[i]
-        inc i
 
 proc make_distance_map*(img: var openarray[byte], width, height : cuint) =
     let sz = (width * height).int
     var data = newSeqDouble(sz)
 
     # Convert img into double (data)
-    var i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         data[i] = img[i].cdouble
-        inc i
 
     make_distance_map(data, width, height)
 
     # Convert back
-    i = 0
-    while i < sz:
+    for i in 0 ..< sz:
         img[i] = (255.byte - data[i].byte)
-        inc i
 
 {.pop.}
